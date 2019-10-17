0c249f4f4d3ad61ee40112dd2a8388a8c3a13b06
likcomet <likcomet@gmail.com>
2019-10-17 19:20:56 +0900

    icecream 1.3 build / Docker 5th build version Modified logrotate

--- Enable-iceccd-scheduler.sh
+++ Enable-iceccd-scheduler.sh
@@ -4,12 +4,12 @@ CPUCOUNT=`nproc`
 touch /var/log/iceccd.log /var/log/icecc-scheduler.log
 chown icecc:icecc /var/log/iceccd.log /var/log/icecc-scheduler.log
 case $ICECREAM_SCHEDULER in
-	yes) icecc-scheduler -d -n neople -l /var/log/icecc-scheduler.log -vvvv
-             iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 3 -n neople -l /var/log/iceccd.log -vvvv;tail -f /var/log/icecc*.log
+	yes) icecc-scheduler -d -n neople-TEST -l /var/log/icecc-scheduler.log -vvvv
+             iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 3 -n neople-TEST -l /var/log/iceccd.log -vvvv;tail -f /var/log/icecc*.log
 	;;
-	no)  iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 3 -n neople -l /var/log/iceccd.log -vvvv;tail -f /var/log/icecc*.log
+	no)  iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 3 -n neople-TEST -l /var/log/iceccd.log -vvvv;tail -f /var/log/icecc*.log
 	;;
 	*)   echo "Null"
 	;;
 esac
-#iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 35 -n neople -l /var/log/icecc.log && tail -f /var/log/icecc.log
+#iceccd -d -s $ICECREAM_SCHEDULER_HOST -m 35 -n neople-TEST -l /var/log/icecc.log && tail -f /var/log/icecc.log
